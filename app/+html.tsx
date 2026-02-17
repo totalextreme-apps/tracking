4e63580adbce630a89309188a46cfd939c63c8b6
MAC1 <mac1@MAC1s-iMac.local>
2026-02-16 22:44:37 -0500

    Update app icon to Tracking logo and add iOS shortcut support

--- app/+html.tsx
+++ app/+html.tsx
@@ -21,6 +21,7 @@ export default function Root({ children }: { children: React.ReactNode }) {
         {/* Using raw CSS styles as an escape-hatch to ensure the background color never flickers in dark-mode. */}
         <style dangerouslySetInnerHTML={{ __html: responsiveBackground }} />
         {/* Add any additional <head> elements that you want globally available on web... */}
+        <link rel="apple-touch-icon" href="/assets/images/logo_tracking.png" />
       </head>
       <body>{children}</body>
     </html>
