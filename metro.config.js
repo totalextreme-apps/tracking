e68ef55580a11f6d87ae10e5c0b0e376cac56e57
totalextreme-apps <ewwarehouse@gmail.com>
2026-08-29 22:15:44 -0400

    Revert expo-image web shim resolver override to restore poster image rendering on web

--- metro.config.js
+++ metro.config.js
@@ -1,22 +1,6 @@
 const { getDefaultConfig } = require('expo/metro-config');
 const { withNativeWind } = require('nativewind/metro');
-const path = require('path');
 
 const config = getDefaultConfig(__dirname);
-
-const originalResolveRequest = config.resolver.resolveRequest;
-
-config.resolver.resolveRequest = (context, moduleName, platform) => {
-  if (platform === 'web' && moduleName === 'expo-image') {
-    return {
-      type: 'sourceFile',
-      filePath: path.resolve(__dirname, 'lib/expo-image-web-shim.tsx'),
-    };
-  }
-  if (originalResolveRequest) {
-    return originalResolveRequest(context, moduleName, platform);
-  }
-  return context.resolveRequest(context, moduleName, platform);
-};
 
 module.exports = withNativeWind(config, { input: './global.css' });
